f19430b8dcf96347f1e0c573c4361469533a5604
prabhusai kotha <kothaprabhusai1998@gmail.com>
2019-03-15 22:17:29 +0530

    Add files via upload

--- TransactionDAOImpl.java
+++ TransactionDAOImpl.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 import com.maven.latestBank.Exception.InsufficientBalanceException;
@@ -31,7 +32,6 @@ public class TransactionDAOImpl implements TransactionDAO{
 			}
 		}
 		
-		
 		if(bal>amount) {
 			bal=bal-amount;
 			String sql1="update customer_details set balance=? where account_no=?";
@@ -42,13 +42,12 @@ public class TransactionDAOImpl implements TransactionDAO{
 		}
 		else
 			throw new InsufficientBalanceException("Balance is insuffiecinet to with draw");
-			
-		
+				
 		return mainui.printBalance(bal);
 	}
 
+	
 	public int deposit(int accountNo,int amount) throws Exception {
-		
 		java.sql.Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/latest_bank","root","mysqlroot1");
 		
 		String sql="select * from customer_details";
@@ -60,24 +59,60 @@ public class TransactionDAOImpl implements TransactionDAO{
 				bal=rs.getFloat(10);
 		}
 		
-		
-		
 		bal=bal+amount;
 		String sql1="update customer_details set balance=? where account_no="+accountNo;
 		PreparedStatement pst1=con.prepareStatement(sql1);
 		pst1.setInt(1,(int) bal);
-		//pst1.setInt(2, accountNo);
 		pst1.executeUpdate();
 		mainui.printBalance(bal);
 		return 0;
 	}
 
-	public int transfer(int accountNo,int amount) {
-		mainui.askAmount();
-		amt=in.nextInt();
+	
+	public int transfer(int fromAccountNo,int toAccountNo,int amount) throws Exception {
+		int c=0,bal;
+		
+		java.sql.Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/latest_bank","root","mysqlroot1");
+		
+		//Validate if toAccountNumber exists or not
+		String sql="select * from customer_details where account_no="+fromAccountNo;
+		PreparedStatement pst2=con.prepareStatement(sql);
+		ResultSet rs=pst2.executeQuery();
+		
+		while(rs.next()) {
+			if(toAccountNo==rs.getInt(1));
+			c=1;
+		}
+		bal=balance(fromAccountNo);
+		if(c==1 && bal>amount) {
+			
+			//Updating fromAccountNo balance
+			String sql1="update customer_details set balance=? where account_no="+fromAccountNo;
+			PreparedStatement pst3=con.prepareStatement(sql1);
+			bal=bal-amount;
+			pst3.setInt(1, bal);
+			pst3.executeUpdate();
+			
+			//Updating fromAccountNo balance
+			String sql2="update customer_details set balance=? where account_no="+toAccountNo;
+			PreparedStatement pst4=con.prepareStatement(sql2);
+			bal=bal+amount;
+			pst4.setInt(1, bal);
+			pst4.executeUpdate();
+			
+			//inserting into transaction details table 
+			String sql3="insert into transaction_details(from_account_no,to_account_no,amount_transfered) values(?,?,?)";
+			PreparedStatement pst5=con.prepareStatement(sql3);
+			pst5.setInt(1, fromAccountNo);
+			pst5.setInt(2, toAccountNo);
+			pst5.setInt(3, amount);
+			pst5.executeUpdate();
+						
+		}
 		return 0;
 	}
 
+	
 	public int balance(int accountNo) throws Exception {
 		java.sql.Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/latest_bank","root","mysqlroot1");
 		
@@ -90,7 +125,7 @@ public class TransactionDAOImpl implements TransactionDAO{
 				bal=rs.getFloat(10);
 			mainui.printBalance(bal);
 		}
-		return 1;
+		return (int) bal;
 	}
 
 }
